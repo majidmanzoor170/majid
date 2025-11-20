ba87c18a0296018efca41d42df504ad635f59039
majidmanzoor170 <165884581+majidmanzoor170@users.noreply.github.com>
2025-11-20 13:28:42 +0000

    Deploying to gh-pages from @ majidmanzoor170/majid@1c87ec6429e72779eea60180e1284a204f5889bf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "A curated collection of my research and development projects, including work on computer vision, autonomous systems, hyperspectral imaging, and efficient deep learning models. These repositories reflect my ongoing efforts in building lightweight, real-time, and deployable AI systems.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/majid/repositories/";
@@ -288,17 +288,6 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/majid/blog/2022/giscus-comments/";
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
       },{id: "post-a-post-with-redirect",
